4ce9bae42a56e5a508e2021eb708d86f0f2f6d8a
Asim <48256968+asim3@users.noreply.github.com>
2021-06-02 20:03:04 +0300

    added: v1 install-and-run-django.sh

---
+++ v1/install-and-run-django.sh
@@ -0,0 +1,8 @@
+cd /your_project_root \
+  && pip3 install -r ./requirements.txt \
+  && python3 manage.py makemigrations \
+  && python3 manage.py migrate \
+  && python3 manage.py runserver 0.0.0.0:8000
+  
+
+# gunicorn --workers 3 --bind 0.0.0.0:8000 my_project.wsgi
